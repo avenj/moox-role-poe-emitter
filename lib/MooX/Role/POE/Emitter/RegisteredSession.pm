1e1bcd50dd65bc3c4a369230e5e5bb7450c2bcd9
Jon Portnoy <avenj@cobaltirc.org>
2013-12-27 19:20:48 -0500

    Cleanups
    	modified:   lib/MooX/Role/POE/Emitter/RegisteredSession.pm

--- lib/MooX/Role/POE/Emitter/RegisteredSession.pm
+++ lib/MooX/Role/POE/Emitter/RegisteredSession.pm
@@ -1,11 +1,7 @@
 package MooX::Role::POE::Emitter::RegisteredSession;
 use Carp;
-
-use Types::Standard -types;
 use Moo;
-
-has id => ( is => 'rw', required => 1 );
-
+has id       => ( is => 'rw', required => 1 );
 has refcount => ( is => 'rw', required => 1 );
 
 1;
